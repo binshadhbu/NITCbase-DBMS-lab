21f364743a8648f70ee7fbf0311078994a7ee29c
binshadhbu <binshadh_b210517cs@nitc.ac.in>
2023-08-08 22:00:52 +0530

    initial one

--- main.cpp
+++ main.cpp
@@ -2,28 +2,12 @@
 #include "Cache/OpenRelTable.h"
 #include "Disk_Class/Disk.h"
 #include "FrontendInterface/FrontendInterface.h"
-#include<iostream>
-using namespace std;
 
 int main(int argc, char *argv[]) {
-   Disk disk_run;
+  /* Initialize the Run Copy of Disk */
+  Disk disk_run;
+  // StaticBuffer buffer;
+  // OpenRelTable cache;
 
-  unsigned char buffer[BLOCK_SIZE];
-  Disk::readBlock(buffer,0);
-  for(int i=0;i<6;i++){
-  cout<<(int)buffer[i];
-  }
-  
-  char message[] = "hello";
-  memcpy(buffer + 20, message, 6);
-  Disk::writeBlock(buffer, 7000);
-  
-  unsigned char buffer2[BLOCK_SIZE];
-  char message2[6];
-  Disk::readBlock(buffer2, 7000);
-  memcpy(message2, buffer2 + 20, 6);
-  std::cout << message2;
-
-
-  return 0;
+  return FrontendInterface::handleFrontend(argc, argv);
 }
